d5c0af2798d3ccc6e125eb5892cf7fce85837f16
Mostafa Wael <mostafa.w.k000@gmail.com>
2022-12-23 13:39:15 -0800

    sensor data

--- src/sensor/scripts/sensor.py
+++ src/sensor/scripts/sensor.py
@@ -4,35 +4,10 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 # import custom message sensor_out.msg
-from sensor_out.msg import SensorOut
+# from sensor_out.msg import SensorOut
 
-# def rearLaserCallback(msg):
-#     # Print the data
-#     rospy.loginfo("Rear Laser")
-# def frontLaserCallback(msg):
-#     # Print the data
-#     rospy.loginfo("Front Laser")
-# def odomCallback(msg):
-#     # Print the data
-#     rospy.loginfo("Odom")
-# if __name__ == '__main__':
-#     rospy.init_node("Summit_XL_Sensor")
-#     rospy.loginfo("Summit XL Sensor Started")
-#     # Define a specific rate to send commands
-#     rate = rospy.Rate(2)
-#     while not rospy.is_shutdown():
-#         # Subscribe to /robot/rear_laser/scan topic
-#         rearLaser = rospy.Subscriber("/robot/rear_laser/scan", LaserScan, callback = rearLaserCallback, queue_size=10)
-#         # Subscribe to /robot/front_laser/scan topic
-#         frontLaser = rospy.Subscriber("/robot/front_laser/scan", LaserScan, callback = frontLaserCallback, queue_size=10)
-#         # Subscribe to /robot/robotnik_base_control/odom topic
-#         odom = rospy.Subscriber("/robot/robotnik_base_control/odom", Odometry, callback = odomCallback, queue_size=10)
-#         rate.sleep()
-# Create a sensor class
 class Sensor:
     def __init__(self):
-        # Define a specific rate to send commands
-        self.rate = rospy.Rate(2)
         # Subscribe to /robot/rear_laser/scan topic
         self.rearLaser = rospy.Subscriber("/robot/rear_laser/scan", LaserScan, callback = self.rearLaserCallback, queue_size=10)
         # Subscribe to /robot/front_laser/scan topic
@@ -46,40 +21,59 @@ class Sensor:
         self.odomData = Odometry()
     def rearLaserCallback(self,msg):
         self.rearLaserData = msg
+        rospy.loginfo('Rear laser data angle_min: %f', self.rearLaserData.angle_min * 180 / 3.14159265359)
+        rospy.loginfo('Rear laser data angle_max: %f', self.rearLaserData.angle_max * 180 / 3.14159265359)
     def frontLaserCallback(self,msg):
         self.frontLaserData = msg
+        rospy.loginfo('Front laser data angle_min: %f', self.frontLaserData.angle_min * 180 / 3.14159265359)
+        rospy.loginfo('Front laser data angle_max: %f', self.frontLaserData.angle_max * 180 / 3.14159265359)
     def odomCallback(self,msg):
         self.odomData = msg
     def combineLaserData(self):
         combinedLaserData = LaserScan()
         if self.rearLaserData == None or self.frontLaserData == None:
             rospy.loginfo('No data')
-            pass
+            return None
         else:
-            combinedLaserData.header.seq = self.rearLaserData.header.seq
-            combinedLaserData.header.stamp = self.rearLaserData.header.stamp
-            combinedLaserData.header.frame_id = self.rearLaserData.header.frame_id
-            combinedLaserData.angle_min = self.rearLaserData.angle_min
-            combinedLaserData.angle_max = self.rearLaserData.angle_max + -(self.frontLaserData.angle_min) + self.frontLaserData.angle_max
-            combinedLaserData.angle_increment = self.rearLaserData.angle_increment
-            combinedLaserData.time_increment = self.rearLaserData.time_increment
+            # order doesn't matter
+            combinedLaserData.header = self.rearLaserData.header
             combinedLaserData.scan_time = self.rearLaserData.scan_time
-            combinedLaserData.range_min = self.rearLaserData.range_min
-            combinedLaserData.range_max = self.rearLaserData.range_max
-            combinedLaserData.ranges = self.rearLaserData.ranges + self.frontLaserData.ranges
-            combinedLaserData.intensities = self.rearLaserData.intensities
-        return combinedLaserData
-    def run(self):
-        while not rospy.is_shutdown():
-            combinedLaserData = self.combineLaserData()
+            combinedLaserData.time_increment = self.rearLaserData.time_increment
+            combinedLaserData.angle_increment = self.rearLaserData.angle_increment
+            combinedLaserData.intensities = self.rearLaserData.intensities # always zero
             
-            self.rate.sleep()
+            combinedLaserData.range_min = min(self.rearLaserData.range_min, self.frontLaserData.range_min)
+            combinedLaserData.range_max = max(self.rearLaserData.range_max, self.frontLaserData.range_max)
+
+            # order matters
+            combinedLaserData.angle_min = self.rearLaserData.angle_min + 3.14159265359
+            combinedLaserData.angle_max = self.rearLaserData.angle_max - 3.14159265359 
+            # print the angle_min and angle_max of the front and rear laser data in degrees
+            
+            
+            # print the angle_min and angle_max of the combined laser data in degrees
+            rospy.loginfo('Combined laser data angle_min: %f', combinedLaserData.angle_min * 180 / 3.14159265359)
+            rospy.loginfo('Combined laser data angle_max: %f', combinedLaserData.angle_max * 180 / 3.14159265359)
+            
+            combinedLaserData.ranges = self.rearLaserData.ranges + self.frontLaserData.ranges
+            # print the len of the front and rear laser data
+            rospy.loginfo('Front laser data len: %d', len(self.frontLaserData.ranges))
+            rospy.loginfo('Rear laser data len: %d', len(self.rearLaserData.ranges))
+            
+        return combinedLaserData
+    def publishLaserData(self):
+        combinedLaserData = self.combineLaserData()
+        if combinedLaserData != None:
+            self.laser.publish(combinedLaserData)
+            
 if __name__ == '__main__':
 
     rospy.init_node("Summit_XL_Sensor")
     rospy.loginfo("Summit XL Sensor Started")
-
+    # Define a specific rate to send commands
+    rate = rospy.Rate(2)
     sensor = Sensor()
-    sensor.run()
-
+    while not rospy.is_shutdown():
+        sensor.publishLaserData()
+        rate.sleep()
     rospy.spin()
