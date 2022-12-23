e3c5d672dfea11498fb8ec7421630a0d2c4e1c88
Mostafa Wael <mostafa.w.k000@gmail.com>
2022-12-23 08:35:43 -0800

    initial setup

---
+++ README.md
@@ -0,0 +1,16 @@
+## How to run
+### Launch Gazebo + rViz
+``` bash
+roslaunch summit_xl_sim_bringup summit_xls_complete.launch
+```
+### List ROS topics related to robot movement commands: 
+``` bash
+rostopic list | grep cmd_vel
+```
+Note: that you should be running the simulator since that's what publishes to
+the topic.
+
+### Control robot movement: 
+``` bash
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py /cmd_vel:=/robot/robotnik_base_control/cmd_vel
+```
---
+++ src/control/scripts/control.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import rospy
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+
+if _name_ == '_main_':
+    rospy.init_node("Summit_XL_Controller")
+    rospy.loginfo("Summit XL Controller Started")
+    # Create a publisher to send commands to the summit_xl
+    pub = rospy.Publisher("/robot/robotnik_base_control/cmd_vel", Twist, queue_size=10)
+    # Define a specific rate to send commands
+    rate = rospy.Rate(2)
+    # Control the robot with WASD keys , if the key is pressed, the robot will move 
+    while not rospy.is_shutdown():
+        twist = Twist()
+        key = input()
+        if key == 'w':
+            twist.linear.x = 1.0
+        elif key == 's':
+            twist.linear.x = -1.0
+        elif key == 'a':
+            twist.angular.z = 1.0
+        elif key == 'd':
+            twist.angular.z = -1.0
+        else:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+        pub.publish(twist)
+        rate.sleep()
