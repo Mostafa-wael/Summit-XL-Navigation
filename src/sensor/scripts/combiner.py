07e69d9d8fee11f63cc9db08597e992dfd3f012d
Mostafa Wael <mostafa.w.k000@gmail.com>
2022-12-31 11:52:49 -0800

    edit combiner

--- src/sensor/scripts/combiner.py
+++ src/sensor/scripts/combiner.py
@@ -2,43 +2,39 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import Pose, Point, Quaternion
 from nav_msgs.msg import Odometry
 from sensor.msg import CombinedSensor
-import math
 import  message_filters
-from math import radians
-import tf
-import tf2_ros
-import numpy as np
 
 
 class Sensor:
     def __init__(self):
-        self.sensorsData = CombinedSensor()    
+        self.sensorsData = CombinedSensor()   
+        # set the header of the combined sensor data
+        self.sensorsData.header.frame_id = "robot_map" 
+        self.sensorsData.header.stamp = rospy.Time.now()
         # create a publisher
         self.sensorPub = rospy.Publisher("/sensors", CombinedSensor, queue_size=1)
         # Subscribe to rear laser topic
         self.laser = message_filters.Subscriber("/scan_multi", LaserScan)
         # Subscribe to odom topic
         self.odom = message_filters.Subscriber("/robot/robotnik_base_control/odom", Odometry)
+        # define the approximate time synchronizer
+        self.synchronizer = message_filters.ApproximateTimeSynchronizer([self.laser, self.odom], 1, 0.0001)
+        self.synchronizer.registerCallback(self.sensorCallback)
     def sensorCallback(self, laserData, odomData):
+        # save the data
         self.sensorsData.laser = laserData
         self.sensorsData.odom = odomData
+        # publish the combined sensor data
         self.sensorPub.publish(self.sensorsData)
-    def combine(self):
-        synchronizer = message_filters.ApproximateTimeSynchronizer([self.laser, self.odom], 1, 0.0001)
-        synchronizer.registerCallback(self.sensorCallback)
 
 
 if __name__ == '__main__':
-
     rospy.init_node('sensors')
     rospy.loginfo('sensor node started')
     rate = rospy.Rate(2)
     sensor = Sensor()
     while not rospy.is_shutdown():
-        sensor.combine()
         rate.sleep()
     rospy.spin()
