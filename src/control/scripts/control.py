0ed246a89f98c5f25676d083f8d7a46a489a7095
Mostafa Wael <mostafa.w.k000@gmail.com>
2022-12-31 11:52:09 -0800

    edit control

--- src/control/scripts/control.py
+++ src/control/scripts/control.py
@@ -9,56 +9,71 @@ import tty
 import termios
 from select import select
 
-def termianl_settings():
-    return termios.tcgetattr(sys.stdin)
 
-def restore_terminal_settings(settings):
-    return termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+# define control class
+class Control:
+    def __init__(self):
+        # define the timeout for the key
+        self.key_timeout = 0.5
+        # Create a publisher to send commands to the summit_xl
+        self.controlPub = rospy.Publisher("/robot/robotnik_base_control/cmd_vel", Twist, queue_size=10)
+        self.twist = Twist()
 
-def get_pressed_char(settings,timeout):
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select([sys.stdin], [], [], timeout)
-    if rlist:
-        key = sys.stdin.read(1)
-    else:
-        key = ''
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+    def getTermianlSettings(self):
+        # get the terminal settings
+        return termios.tcgetattr(sys.stdin)
 
+    def restoreTerminalSettings(self, settings):
+        # restore the terminal settings
+        return termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
-if __name__ == '__main__':
-    rospy.init_node("Summit_XL_Controller")
-    rospy.loginfo("Summit XL Controller Started")
-    # Create a publisher to send commands to the summit_xl
-    pub = rospy.Publisher("/robot/robotnik_base_control/cmd_vel", Twist, queue_size=10)
-    # Define a specific rate to send commands
-    rate = rospy.Rate(50)
-    key_timeout = 0.5
+    def getPressedChar(self, settings,timeout):
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select([sys.stdin], [], [], timeout)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
     # Control the robot with WASD keys , if the key is pressed, the robot will move 
-    twist = Twist()
-    while not rospy.is_shutdown():
-        settings = termianl_settings()
-        key = get_pressed_char(settings, key_timeout)
+    def handleMotion(self):
+        settings = self.getTermianlSettings()
+        key = self.getPressedChar(settings, self.key_timeout)
         try:
             # if w is pressed, the robot will move forward and increase the linear velocity
             if key == 'w':
-                twist.linear.x += 0.2
+                self.twist.linear.x += 0.1
             # if s is pressed, the robot will move backward and decrease the linear velocity
             elif key == 's':
-                twist.linear.x -= 0.2
+                self.twist.linear.x -= 0.1
             # if a is pressed, the robot will move left and increase the angular velocity
             elif key == 'a':
-                twist.angular.z += 0.2
+                self.twist.angular.z += 0.1
             # if d is pressed, the robot will move right and decrease the angular velocity
             elif key == 'd':
-                twist.angular.z -= 0.2
+                self.twist.angular.z -= 0.1
             # else decrease the linear and angular velocity until it reaches 0
             else:
-                twist.linear.x *= 0.5
-                twist.angular.z *= 0.5
+                self.twist.linear.x *= 0.1
+                self.twist.angular.z *= 0.1
             # publish the command to the robot
-            pub.publish(twist)
-            rate.sleep()
+            self.controlPub.publish(self.twist)
         finally:
-            restore_terminal_settings(settings)
+            self.restoreTerminalSettings(settings)
+
+
+if __name__ == '__main__':
+    rospy.init_node("Controller")
+    rospy.loginfo("Summit XL Controller Started")
+    # Define a specific rate to send commands
+    rate = rospy.Rate(50)
+    # Create an object from the control class
+    control = Control()
+    while not rospy.is_shutdown():
+        # call the handleMotion function
+        control.handleMotion()
+        rate.sleep()
+    rospy.spin()
+       
 
